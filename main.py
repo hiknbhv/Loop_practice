e96bae7fdcc63b76e68747a8771dee0845d315cb
Tamilea pimpton <pimptam0000@warrren.k12.in.us>
2024-11-04 09:24:17 -0500

    commit

---
+++ main.py
@@ -0,0 +1,17 @@
+my_sum = 0
+for number in range(1, 101):
+    if number % 2 == 0:
+        my_sum += number
+
+print("Sum of even number from 1 to 100 is:", my_sum)
+charlie_sum = 0
+brown_sum = 0
+for number in range(1,301):
+    if number % 7 == 0:
+        print ("Charlie")
+    elif number % 3 == 0:
+        print ("Brown")
+    else:
+        print(number)
+
+
